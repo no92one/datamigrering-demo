1c0d657930adbe10525b7e3de15fde16d2dea0c9
unknown <linus.lindroth.92@gmail.com>
2025-05-21 11:49:08 +0200

    added check, tranasaction, ETL

--- app.py
+++ app.py
@@ -1,19 +1,37 @@
 from db import init_db, SessionLocal
 from models import Customer, Account
+from sqlalchemy.exc import SQLAlchemyError
+import csv
 
 def main():
     init_db()
     db = SessionLocal()
 
-    customer = Customer(name="Benjamin", ssn="7001092456", email="benjamin@example.com")
-    db.add(customer)
-    db.commit()
+    #try:
+        #with db.begin():
+            #with open('customers.csv', newline='') as csvfile:
+                #reader = csv.DictReader(csvfile)
+                #for index, row in enumerate(reader):
+                    #customer = Customer(name=row["name"], ssn=row["ssn"], email=row["email"])
+                    #db.add(customer)
 
-    account = Account(number="8064047892", balance=200, customer_id=customer.id)
-    db.add(account)
-    db.commit()
+    #except SQLAlchemyError as e:
+        #db.rollback()
+        #print("[ERROR] Något gick fel:", e)
 
-    print(f"{customer.name} ({customer.email}) har saldo {account.balance}")
+    #try:
+        #with db.begin():
+           #customer = Customer(name="Benjamin", ssn="7001092456", email="benjamin@example.com")
+            #db.add(customer)
+
+            #account = Account(number="8064047892", balance=200, customer_id=customer.id)
+            #db.add(account)
+
+    #except SQLAlchemyError as e:
+        #db.rollback()
+        #print("[ERROR] Något gick fel:", e)
+
+    #print(f"{customer.name} ({customer.email}) har saldo {account.balance}")
 
     db.close()
 
--- models.py
+++ models.py
@@ -1,20 +1,29 @@
-from sqlalchemy import Column, Integer, String, Numeric, ForeignKey
+from sqlalchemy import Column, Integer, String, Numeric, ForeignKey, CheckConstraint
 from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
 class Customer(Base):
     __tablename__ = 'customers'
+
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     ssn = Column(String, nullable=False, unique=True)
-    accounts = relationship('Account', back_populates='customer')
     email = Column(String)
+
+    accounts = relationship('Account', back_populates='customer')
 
 class Account(Base):
     __tablename__ = 'accounts'
+
     id = Column(Integer, primary_key=True)
     number = Column(String, unique=True)
     balance = Column(Numeric, default=0, nullable=False)
     customer_id = Column(Integer, ForeignKey("customers.id"))
+
+    __table_args__ = (
+        CheckConstraint("balance >= 0", name="balance_not_negative_check"),
+        CheckConstraint("balance <= 5000000", name="max_balance_check"),
+    )
+
     customer = relationship("Customer", back_populates="accounts")
